0de6dd983a8a39710ccf683c891e5e5d54104c2e
vanshika1604 <91271424+vanshika1604@users.noreply.github.com>
2022-07-25 19:56:40 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>Here is the todo list.
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+    </div>
+  )
+}
